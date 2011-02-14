41b0d20bdfd4225fa763c525210687deaa343d9a
Eric Kidd <git@randomhacks.net>
2011-02-14 14:06:44 -0500

    Test basic graph pattern matches
    The doap.nt file was copied from rdf-spec, which is in the public
    domain.

--- spec/agraph_spec.rb
+++ spec/agraph_spec.rb
@@ -11,6 +11,12 @@ require 'rdf/spec/repository'
 require 'rdf'
 require 'rdf/agraph'
 
+RSpec::Matchers.define :include_solution do |hash|
+  match do |solutions|
+    solutions.any? {|s| s.to_hash == hash }
+  end
+end
+
 describe RDF::AllegroGraph::Repository do
   before :each do
     options = {
@@ -25,6 +31,32 @@ describe RDF::AllegroGraph::Repository do
     @repository.clear
   end
 
-  # @see lib/rdf/spec/repository.rb
   it_should_behave_like RDF_Repository
+
+  context "with example data" do
+    before :each do
+      path = File.join(File.dirname(__FILE__), '..', 'etc', 'doap.nt')
+      @repository.load(path)
+    end
+
+    describe ".query" do
+      it "match a Basic Graph Patterns" do
+        query = RDF::Query.new do |q|
+          q.pattern [:person, RDF.type, RDF::FOAF.Person]
+          q.pattern [:person, RDF::FOAF.name, :name]
+          q.pattern [:person, RDF::FOAF.mbox, :email]
+        end
+        s = @repository.query(query)
+        s.should include_solution(:person => "http://ar.to/#self",
+                                  :name => "Arto Bendiken",
+                                  :email => "mailto:arto.bendiken@gmail.com")
+        s.should include_solution(:person => "http://bhuga.net/#ben",
+                                  :name => "Ben Lavender",
+                                  :email => "mailto:blavender@gmail.com")
+        s.should include_solution(:person => "http://kellogg-assoc.com/#me",
+                                  :name => "Gregg Kellogg",
+                                  :email => "mailto:gregg@kellogg-assoc.com")
+      end
+    end
+  end
 end
